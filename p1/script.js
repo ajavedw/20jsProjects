ba6664b00be5cb169529f72796db12a67dbed4e3
adnanj <adnan.webdev@gmail.com>
2020-03-22 03:44:03 +1100

    p1

---
+++ p1/script.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function checkInput(inputArr){
+    inputArr.forEach(function(input) {
+        if (input.value.trim()===''){
+            showError(input, `${getFieldName(input)} is required`);
+        }
+        else {
+            showSuccess(input);
+        }
+
+    });
+}
+
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function validateEmail(email){
+    const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    return re.test(String(email).toLowerCase());
+
+}
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  checkInput([username,email,password,password2]);
+
+});
